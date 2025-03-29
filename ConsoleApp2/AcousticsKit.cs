9251f35432e7e72926fa4d8a9052147347ac598f
xXXTylerDurdenXXx <143999505+xXXTylerDurdenXXx@users.noreply.github.com>
2025-03-29 09:02:47 +0300

    Добавил акустическую систему

---
+++ ConsoleApp2/AcousticsKit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class AcousticsKit
+    {
+        public void On() => Console.WriteLine("Акустическая система включена");
+        public void Off() => Console.WriteLine("Акустическая система выключена");
+
+        public void ConnectionAmplifier(Amplifier amplifier) => Console.WriteLine($"К акустической системе подключен усилитель");
+
+        public void OffConnectionAmplifier() => Console.WriteLine($"Усилитель отключен от акустической системы");
+
+    }
+}
--- ConsoleApp2/HomeTheaterFacade.cs
+++ ConsoleApp2/HomeTheaterFacade.cs
@@ -11,12 +11,14 @@ namespace ConsoleApp2
         private Amplifier _amplifier;
         private DVDPlayer _dvdPlayer;
         private Projector _projector;
+        private AcousticsKit _acousticsKit;
 
-        public HomeTheaterFacade(Amplifier amplifier, DVDPlayer dvdPlayer, Projector projector)
+        public HomeTheaterFacade(Amplifier amplifier, DVDPlayer dvdPlayer, Projector projector, AcousticsKit acousticsKit)
         {
             _amplifier = amplifier;
             _dvdPlayer = dvdPlayer;
             _projector = projector;
+            _acousticsKit = acousticsKit;
         }
 
         public void WatchMovie(string movie)
@@ -26,6 +28,8 @@ namespace ConsoleApp2
             _projector.SetInput("DVD");
             _amplifier.On();
             _amplifier.SetVolume(5);
+            _acousticsKit.On();
+            _acousticsKit.ConnectionAmplifier(_amplifier);
             _dvdPlayer.On();
             _dvdPlayer.Play(movie);
         }
@@ -36,6 +40,8 @@ namespace ConsoleApp2
             _dvdPlayer.Stop();
             _dvdPlayer.Off();
             _amplifier.Off();
+            _acousticsKit.OffConnectionAmplifier();
+            _acousticsKit.Off();
             _projector.Off();
         }
     }
