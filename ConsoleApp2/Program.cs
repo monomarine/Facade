5f926e3e0c79207f23d3ccad218b774e0d9087f9
xXXTylerDurdenXXx <143999505+xXXTylerDurdenXXx@users.noreply.github.com>
2025-03-29 09:12:00 +0300

    Анпилогов

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,8 +7,9 @@
             Amplifier amplifier = new Amplifier();
             DVDPlayer dvdPlayer = new DVDPlayer();
             Projector projector = new Projector();
+            AcousticsKit acousticsKit = new AcousticsKit();
 
-            HomeTheaterFacade homeTheater = new HomeTheaterFacade(amplifier, dvdPlayer, projector);
+            HomeTheaterFacade homeTheater = new HomeTheaterFacade(amplifier, dvdPlayer, projector, acousticsKit);
 
             homeTheater.WatchMovie("Гарри Поттер и принц-полукровка");
             homeTheater.EndMovie();
