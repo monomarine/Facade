e7e6b66e88094ccbe713c2208e4d142c5fbc0a8c
danilova veronika <dveronika682@gmail.com>
2025-12-25 11:57:19 +0500

    Данилова, добавлен SoundSystem

--- ConsoleApp2/HomeTheaterFacade.cs
+++ ConsoleApp2/HomeTheaterFacade.cs
@@ -8,24 +8,37 @@ namespace ConsoleApp2
 {
     internal class HomeTheaterFacade
     {
-        private Amplifier _amplifier;
-        private DVDPlayer _dvdPlayer;
-        private Projector _projector;
+        private readonly Amplifier _amplifier;
+        private readonly DVDPlayer _dvdPlayer;
+        private readonly Projector _projector;
+        private readonly SoundSystem _soundSystem; 
 
-        public HomeTheaterFacade(Amplifier amplifier, DVDPlayer dvdPlayer, Projector projector)
+        public HomeTheaterFacade(
+            Amplifier amplifier,
+            DVDPlayer dvdPlayer,
+            Projector projector,
+            SoundSystem soundSystem) 
         {
             _amplifier = amplifier;
             _dvdPlayer = dvdPlayer;
             _projector = projector;
+            _soundSystem = soundSystem; 
         }
 
         public void WatchMovie(string movie)
         {
             Console.WriteLine("Приготовься к просмотру фильма...");
+
             _projector.On();
             _projector.SetInput("DVD");
+
+            _soundSystem.On();              
+            _soundSystem.SetMode("Кино");   
+            _soundSystem.SetVolume(7);      
+
             _amplifier.On();
             _amplifier.SetVolume(5);
+
             _dvdPlayer.On();
             _dvdPlayer.Play(movie);
         }
@@ -33,9 +46,13 @@ namespace ConsoleApp2
         public void EndMovie()
         {
             Console.WriteLine("Заканчиваем фильм...");
+
             _dvdPlayer.Stop();
             _dvdPlayer.Off();
+
             _amplifier.Off();
+
+            _soundSystem.Off(); 
             _projector.Off();
         }
     }
---
+++ ConsoleApp2/SoundSystem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class SoundSystem
+    {
+        public void On() => Console.WriteLine("Звуковая система включена");
+        public void Off() => Console.WriteLine("Звуковая система выключена");
+
+        public void SetMode(string mode) => Console.WriteLine($"Режим звука: {mode}");
+        public void SetVolume(int level) => Console.WriteLine($"Громкость звуковой системы: {level}");
+    }
+}
